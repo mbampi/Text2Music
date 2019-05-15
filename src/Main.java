f6375634f0636566a8365d12ba7e20656ce87cbb
Matheus Dussin Bampi <matheusbampi@gmail.com>
2019-05-14 22:20:37 -0300

    imported JFugue

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+
+import org.jfugue.pattern.Pattern;
+import org.jfugue.player.Player;
+
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Java Text2Music!");
+
+        Player player = new Player();
+        Pattern pattern = new Pattern("C D E F G A B");
+        player.play(pattern);
     }
+
+
 }
